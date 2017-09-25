ce4547fae18b26dd80216e8237b7a3268e13ec02
Raphael H Ribas <raphael.ribas@gmail.com>
2017-09-25 06:13:24 -0700

    Reduce default test time.

--- eval.sh
+++ eval.sh
@@ -6,4 +6,4 @@ set -x
 make -j4
 cp sawbot sawbot-candidate 
 
-exec ./test/test.py --config "config.json" --count 1000 --logdir logs/ --time-per-move 100 "$@"
+exec ./test/test.py --config "config.json" --count 1000 --logdir logs/ "$@"
